bc6700b2192a18bdaf730b9b938d6665222076c4
Garvit Joshi <garvitjoshi46@gmail.com>
2025-06-08 13:43:09 +0530

    quick changes in setup

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div className="h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">Consistency App 🚀</h1>
-    </div>
-  )
-}
